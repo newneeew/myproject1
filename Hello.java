5061b278c59eb3e471b0b8835bae03a2f562745b
newneeew <fresh_newnew@naver.com>
2024-11-25 13:29:36 +0900

    Hello.java 추가

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello Java 1");
+		System.out.println("Hello Java 2");
+	}
+}
